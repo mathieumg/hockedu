9a561d97bb00d7ef7ef6a9e0d237b7686ee1f09e
Mathieu M-G <mathieu.marengere-gosselin@polymtl.ca>
2013-01-31 21:22:23 -0500

    Version fonctionnelle du prototype. (Refs #1526)

--- trunk/Sources/ObjC/PrototypeImagePicker/ImagePicker/ImagePicker/ViewController.m
+++ trunk/Sources/ObjC/PrototypeImagePicker/ImagePicker/ImagePicker/ViewController.m
@@ -32,7 +32,7 @@
     
     picker = [[UIImagePickerController alloc] init];
     picker.delegate = self;
-        
+    
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
         
     {
@@ -49,7 +49,7 @@
     
     popOverController = [[UIPopoverController alloc] initWithContentViewController:picker];
     popOverController.delegate = self;
-
+    
     
     //[self presentViewController:picker animated:YES completion:nil];
     [popOverController presentPopoverFromRect:CGRectMake(0.0, 0.0, 400.0, 200.0) inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
@@ -62,40 +62,134 @@
     UIImage* image = [info valueForKey:@"UIImagePickerControllerOriginalImage"];
     
     selectedImage.image = image;
-    [self dismissViewControllerAnimated:YES completion:nil];
+    
     
     // Get the data for the image as a PNG
-    NSData* imageData = UIImagePNGRepresentation(image);
-    
+    //NSData* imageData = UIImagePNGRepresentation(image);
     // Give a name to the file
-    NSString* imageName = @"MyImage.png";
+    //NSString* imageName = @"MyImage.png";
     
     // Now, we have to find the documents directory so we can save it
     // Note that you might want to save it elsewhere, like the cache directory,
     // or something similar.
-    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString* documentsDirectory = [paths objectAtIndex:0];
+    //NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    //NSString* documentsDirectory = [paths objectAtIndex:0];
     
     // Now we get the full path to the file
-    NSString* fullPathToFile = [documentsDirectory stringByAppendingPathComponent:imageName];
+    //NSString* fullPathToFile = [documentsDirectory stringByAppendingPathComponent:imageName];
     
     // and then we write it out
-    [imageData writeToFile:fullPathToFile atomically:NO];
+    //[imageData writeToFile:fullPathToFile atomically:NO];
     
     
-    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:fullPathToFile];
+    //BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:fullPathToFile];
     
     
-    if(fileExists)
-    {
-    UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Error"
-                                                         message:@"Oui"
+    /*if(fileExists)
+     {
+     UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Error"
+     message:@"Oui"
+     delegate:nil
+     cancelButtonTitle:@"OK"
+     otherButtonTitles:nil];
+     [errorAlert show];
+     }
+     */
+    
+    // Dictionary that holds post parameters. You can set your post parameters that your server accepts or programmed to accept.
+    NSMutableDictionary* _params = [[NSMutableDictionary alloc] init];
+    
+    [_params setObject:[NSString stringWithFormat:@"%d",image.imageOrientation] forKey:@"orientation"];
+    //[_params setObject:[NSString stringWithString:@"en"] forKey:[NSString stringWithString:@"lan"]];
+    //[_params setObject:[NSString stringWithFormat:@"%d", userId] forKey:[NSString stringWithString:@"userId"]];
+    //[_params setObject:[NSString stringWithFormat:@"%@",title] forKey:[NSString stringWithString:@"title"]];
+    
+    
+    
+    // the boundary string : a random string, that will not repeat in post data, to separate post data fields.
+    NSString *BoundaryConstant = @"----------V2ymHFg03ehbqgZCaKO6jy";
+    
+    // string constant for the post parameter 'file'. My server uses this name: `file`. Your's may differ
+    NSString* FileParamConstant = @"myfile";
+    
+    // the server url to which the image (or the media) is uploaded. Use your server url here
+    //NSURL* requestURL = [NSURL URLWithString:@"http://posttestserver.com/post.php"];
+    NSURL* requestURL = [NSURL URLWithString:@"http://www.hockedu.com/proto.php"];
+    
+    
+    // create request
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
+    [request setHTTPShouldHandleCookies:NO];
+    [request setTimeoutInterval:600];
+    [request setHTTPMethod:@"POST"];
+    
+    // set Content-Type in HTTP header
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", BoundaryConstant];
+    [request setValue:contentType forHTTPHeaderField: @"Content-Type"];
+    
+    // post body
+    NSMutableData *body = [NSMutableData data];
+    
+    // add params (all params are strings)
+    for (NSString *param in _params) {
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", param] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"%@\r\n", [_params objectForKey:param]] dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    // add image data
+    NSData *imageData = UIImageJPEGRepresentation(image, 1.0);\
+    
+    NSLog(@"%d", image.imageOrientation);
+    
+    if (imageData) {
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"test.jpg\"\r\n", FileParamConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[@"Content-Type: image/jpeg\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:imageData];
+        [body appendData:[[NSString stringWithFormat:@"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    // setting the body of the post to the reqeust
+    [request setHTTPBody:body];
+    
+    // set the content-length
+    NSString *postLength = [NSString stringWithFormat:@"%d", [body length]];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    
+    // set URL
+    [request setURL:requestURL];
+    
+    NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+    
+    NSLog(@"Image Return String: %@", returnString);
+    
+    
+    // JSON parsing.
+    NSError* error;
+    NSDictionary* json = [NSJSONSerialization
+                          JSONObjectWithData:returnData //1
+                          
+                          options:kNilOptions
+                          error:&error];
+    
+    
+    // On a le url ici!! de limage fraichement uploader
+    NSArray* imageLocation = [json objectForKey:@"location"]; //2
+    
+    NSLog(@"location: %@", imageLocation); //3
+    UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Upload successful!"
+                                                         message:[NSString stringWithFormat:@"Image location: %@", imageLocation]
                                                         delegate:nil
                                                cancelButtonTitle:@"OK"
                                                otherButtonTitles:nil];
     [errorAlert show];
-        }
     
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 -(void)imagePickerControllerDidCancel:
