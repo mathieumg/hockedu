cfb03ac83443b3d65634c1df1931561633942223
Samuel Ledoux <samuel.ledoux@polymtl.ca>
2013-02-13 17:10:12 -0500

    Ajout dune interface avec canvas resizer

---
+++ trunk/Sources/ObjC/InterfaceMockup/AppDemo/EAGLViewController.h
@@ -0,0 +1,43 @@
+//
+//  EAGLViewController.h
+//  AppDemo
+//
+
+#import <UIKit/UIKit.h>
+
+#import <OpenGLES/EAGL.h>
+
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+#import "OpenGLWaveFrontObject.h"
+
+extern float const LARGEUR_FENETRE;
+extern float const HAUTEUR_FENETRE;
+
+@class EAGLView;
+
+@interface EAGLViewController : UIViewController <UIGestureRecognizerDelegate> {
+    EAGLContext *context;
+    EAGLView* theEAGLView;
+    GLuint program;
+    IBOutlet UIView *mGLView;
+    
+    BOOL animating;
+    NSInteger animationFrameInterval;
+    CADisplayLink *displayLink;
+    
+    OpenGLWaveFrontObject *cube;
+}
+
+@property (readonly, nonatomic, getter=isAnimating) BOOL animating;
+@property (nonatomic) NSInteger animationFrameInterval;
+@property (nonatomic, retain) OpenGLWaveFrontObject *cube;
+
+- (void)startAnimation;
+- (void)stopAnimation;
+- (void)setupView;
+- (void)rotationDetectee:(UIGestureRecognizer *)gestureRecognizer;
+
+@end
