ea553f6f3f931ab9aec9d8b3ad7222266e7eb570
Mathieu Parent <mathieu-2.parent@polymtl.ca>
2013-03-02 14:59:24 -0500

    refs #2953 Ajout de l'entree de paramatres au demarrage du serveur jeu et du serveur maitre (J'ai fait les 2 tant qu'a faire)

--- trunk/Sources/C++/ServeurMaitre/mainServeurMaitre.cpp
+++ trunk/Sources/C++/ServeurMaitre/mainServeurMaitre.cpp
@@ -6,10 +6,89 @@
 #include "../Reseau/PaquetHandlers/PacketHandlerChatMessage.h"
 #include "../Reseau/UsinePaquets/UsinePaquetChatMessage.h"
 #include "FacadeServeurMaitre.h"
+#include "CommunicateurBD.h"
 
-int main(void)  {
+char flagChar = '-';
+
+
+
+// Fonction pour les flags qui ont un string apres la flag
+// Ex: -m 192.168.0.0.1
+void parseParamWithArg(std::string& pOutParam, int pLoopCounter, int argc, char* argv[])
+{
+    if(pLoopCounter == argc-1) // Si dernier arg, erreur
+    {
+        throw std::runtime_error("Valeur manquante pour le flag " + std::string(argv[pLoopCounter]));
+    }
+
+    // Flag pour donner l'adresse du serveur maitre
+    pOutParam = argv[pLoopCounter+1];
+
+    if(pOutParam[0] == flagChar)
+    {
+        throw std::runtime_error("Valeur manquante pour le flag " + std::string(argv[pLoopCounter]));
+    }
+}
+
+int main(int argc, char* argv[])  {
 
     std::cout << "--------- Serveur Maitre ---------" << std::endl << std::endl;
+
+    //********** Liste des flags
+    // -bHost       Hostname de la BD
+    // -bName       Nom de la BD
+    // -bUser       Username a utiliser pour la connexion a la BD
+    // -bPassword   Password a utiliser pour la connexion a la BD
+    //**********
+    std::string wBDHost     = "hockedu.com:3306";
+    std::string wBDUser     = "mapary";
+    std::string wDBPassword = "mz41wo13";
+    std::string wDBName     = "poly_hockedu";
+
+
+    try
+    {
+        for(int i = 0; i < argc; i++) 
+        {
+            if(argv[i][0] == flagChar) // Si flag
+            {
+                std::string wFlag = argv[i]+1; // On laisse tomber le premier char car c'est le flagchar
+                
+                if(wFlag == "bHost")
+                {
+                    parseParamWithArg(wBDHost, i, argc, argv);
+                }
+                else if(wFlag == "bName")
+                {
+                    parseParamWithArg(wDBName, i, argc, argv);
+                }
+                else if(wFlag == "bUser")
+                {
+                    parseParamWithArg(wBDUser, i, argc, argv);
+                }
+                else if(wFlag == "bPassword")
+                {
+                    parseParamWithArg(wDBPassword, i, argc, argv);
+                }
+                else
+                {
+                    throw std::runtime_error("Flag invalide");
+                }
+                ++i;
+            }
+        }
+    }
+    catch(std::runtime_error& e)
+    {
+        std::cout << "Erreur dans les parametres" << std::endl;
+        std::cout << e.what() << std::endl;
+        return -1;
+    }
+
+    // Setup CommunicateurBD
+    CommunicateurBD::setup(wBDHost, wDBName, wBDUser, wDBPassword);
+
+
 
     GestionnaireReseauServeurMaitre::obtenirInstance();
 	GestionnaireReseau* wGestReseau = GestionnaireReseau::obtenirInstance();
--- trunk/sources/c++/ServeurJeu/mainServeurJeu.cpp
+++ trunk/sources/c++/ServeurJeu/mainServeurJeu.cpp
@@ -3,12 +3,82 @@
 #include <iostream>
 #include <sstream>
 #include <wtypes.h>
+#include <stdexcept>
 
 typedef void (*FunctionFunc)();
+char flagChar = '-';
 
-int main(void)  {
+
+
+// Fonction pour les flags qui ont un string apres la flag
+// Ex: -m 192.168.0.0.1
+void parseParamWithArg(std::string& pOutParam, int pLoopCounter, int argc, char* argv[])
+{
+    if(pLoopCounter == argc-1) // Si dernier arg, erreur
+    {
+        throw std::runtime_error("Valeur manquante pour le flag " + std::string(argv[pLoopCounter]));
+    }
+
+    // Flag pour donner l'adresse du serveur maitre
+    pOutParam = argv[pLoopCounter+1];
+
+    if(pOutParam[0] == flagChar)
+    {
+        throw std::runtime_error("Valeur manquante pour le flag " + std::string(argv[pLoopCounter]));
+    }
+}
+
+
+
+int main(int argc, char* argv[])  {
 
     std::cout << "--------- Serveur Jeu ---------" << std::endl << std::endl;
+
+    
+
+    std::string wMasterServerIP = "173.177.0.193"; // Adresse par defaut du serveur maitre
+
+    
+    //********** Liste des flags
+    // -mIP     Adresse IPv4 du serveur Maitre
+    //**********
+
+
+    try
+    {
+        for(int i = 0; i < argc; i++) 
+        {
+            if(argv[i][0] == flagChar) // Si flag
+            {
+                std::string wFlag = argv[i]+1; // On laisse tomber le premier char car c'est le flagchar
+                
+                if(wFlag == "mIP")
+                {
+                    parseParamWithArg(wMasterServerIP, i, argc, argv);
+                }
+                else
+                {
+                    throw std::runtime_error("Flag invalide");
+                }
+                ++i;
+            }
+        }
+    }
+    catch(std::runtime_error& e)
+    {
+        std::cout << "Erreur dans les parametres" << std::endl;
+        std::cout << e.what() << std::endl;
+        return -1;
+    }
+
+
+
+    // On utilise les params de flag
+
+
+
+
+
     HINSTANCE wDLL = LoadLibrary(TEXT("RazerGame.dll"));
 
     if (!wDLL)
