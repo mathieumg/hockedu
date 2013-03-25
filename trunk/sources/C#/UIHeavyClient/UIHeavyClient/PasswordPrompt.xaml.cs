fdff23d037a784f63b14ace8b6be758c1fcff779
vilema <vincent.lemire@polymtl.ca>
2013-03-25 14:30:52 -0400

    refs #3382 @3h00 &Interfaces et différent popup pour le online lobby.&

---
+++ trunk/sources/C#/UIHeavyClient/UIHeavyClient/PasswordPrompt.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UIHeavyClient
+{
+    /// <summary>
+    /// Logique d'interaction pour PasswordPrompt.xaml
+    /// </summary>
+    public partial class PasswordPrompt : Window
+    {
+        private bool mOkIsClicked;
+
+        public string Password
+        {
+            get { return mPasswordBox.Password; }
+        }
+        public bool OkIsClicked
+        {
+            get { return mOkIsClicked; }
+        }
+
+        public PasswordPrompt()
+        {
+            InitializeComponent();
+            mOkIsClicked = false;
+        }
+
+        public void ClearInput()
+        {
+            mPasswordBox.Clear();
+        }
+
+        private void mCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            mOkIsClicked = false;
+            Close();
+        }
+
+        private void mOkButton_Click(object sender, RoutedEventArgs e)
+        {
+            mOkIsClicked = true;
+            Close();
+        }
+    }
+}
