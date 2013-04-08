d1674819f4ba49aa0485b769e8751505f51c21b6
Michael Ferris <michael.ferris@polymtl.ca>
2013-04-08 04:26:11 -0400

    pan gesture

--- trunk/Sources/ObjC/InterfaceMockup/AppDemo/EAGLViewController.m
+++ trunk/Sources/ObjC/InterfaceMockup/AppDemo/EAGLViewController.m
@@ -327,6 +327,11 @@ enum {
     
     UISwipeGestureRecognizer *swipeGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeDetected:)];
     
+    UIPanGestureRecognizer* panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panDetected:)];
+    
+    panGesture.maximumNumberOfTouches = 2;
+    panGesture.minimumNumberOfTouches = 2;
+    
     [rotationGesture setDelegate:self];
     //[longPressGesture setDelegate:self];
     //[longPressGesture setNumberOfTapsRequired:2];
@@ -334,11 +339,13 @@ enum {
     //[longPressGesture setCancelsTouchesInView:YES];
     [pinchGesture setDelegate:self];
     [swipeGesture setDelegate:self];
+    [panGesture setDelegate:self];
     
     [mGLView addGestureRecognizer:rotationGesture];
     //[mGLView addGestureRecognizer:longPressGesture];
     [mGLView addGestureRecognizer:longPressGesture];
     [mGLView addGestureRecognizer:pinchGesture];
+    [mGLView addGestureRecognizer:panGesture];
     [carousel addGestureRecognizer:swipeGesture];
     
     [rotationGesture release];
@@ -346,6 +353,7 @@ enum {
     [longPressGesture release];
     [pinchGesture release];
     [swipeGesture release];
+    [panGesture release];
     
     // FIN SETUP DES GESTURES
     
@@ -1154,6 +1162,14 @@ enum {
     }
     
     
+}
+
+- (IBAction)panDetected:(UIPanGestureRecognizer*)sender
+{
+    CGPoint translation = [sender translationInView:theEAGLView];
+    [sender setTranslation:translation inView:theEAGLView];
+    
+    [mModel orbit:translation.x :translation.y];
 }
 
 - (IBAction)swipeDetected:(id)sender
