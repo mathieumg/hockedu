c93bb9dd1adc1efc2034fb59b680d873c23f1173
Charles Etienne Lalonde <charles-etienne.lalonde@polymtl.ca>
2013-04-16 21:09:43 -0400

    Load la bonne map pour la simualtion des AIs Début de tentatives de run 2 games en même temps.

--- trunk/Sources/C++/Jeu/Partie.cpp
+++ trunk/Sources/C++/Jeu/Partie.cpp
@@ -38,7 +38,7 @@
 
 
 GLuint Partie::listePause_ = 0;
-const int Partie::POINTAGE_GAGNANT = 20000;
+const int Partie::POINTAGE_GAGNANT = 1000;
 
 
 ////////////////////////////////////////////////////////////////////////
